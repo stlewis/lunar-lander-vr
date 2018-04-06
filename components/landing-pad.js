4d4f12f40e741b9a0b015537173778dfd079b98d
Steve Lewis <steve@decodingsteve.com>
2018-04-06 12:37:52 -0700

    Initial commit after moving from experiments

---
+++ components/landing-pad.js
@@ -0,0 +1,25 @@
+AFRAME.registerComponent('landing-pad', {
+
+  init: function() {
+    this.onCollision = this.onCollision.bind(this);
+    this.scoreHUD = document.querySelector('#score-hud');
+    this.el.addEventListener('collide', this.onCollision);
+    this.touched = false;
+  },
+
+  onCollision: function(e) {
+    this.score = parseInt(document.querySelector('a-scene').getAttribute('score'))
+
+    var contactNormal = e.detail.contact.ni
+    var verticalContact = contactNormal.y
+
+    if(verticalContact <= -0.5 && !this.touched ) {
+      this.el.setAttribute('color', 'green');
+      this.touched = true;
+      this.score += 10
+      document.querySelector('a-scene').setAttribute('score', this.score)
+      this.scoreHUD.setAttribute('text', {value: "Score: " +  this.score})
+    }
+  }
+
+});
---
+++ components/thrusters.js
@@ -0,0 +1,43 @@
+AFRAME.registerComponent('thrusters', {
+  schema: {
+    fuelLevel: {default: 500 },
+    thrusterVelocityMax: {default: 5 },
+  },
+
+  init: function() {
+    this.keyboardControls = this.el.components['keyboard-controls'];
+    this.movementControls = this.el.components['movement-controls'];
+    this.body             = this.el.components['kinematic-body'].body;
+    this.fuelHUD = document.querySelector('#fuel-hud');
+    this.speedHUD = document.querySelector("#speed-hud")
+    this.altitudeHUD = document.querySelector("#altitude-hud")
+    // We also need to decide what the "safe" landing range in in terms of speed.
+    this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+    this.speedHUD.setAttribute('text', { value: "Vertical Speed: 0"});
+    this.altitudeHUD.setAttribute('text', { value: "AGL: 0"});
+
+    this.thrusterEngagedSecs = 0;
+  },
+
+  tick: function(globalUp, timeDelta) {
+    this.speedHUD.setAttribute('text', {value: "Vertical Speed: " + Math.round(this.body.velocity.y) })
+    this.altitudeHUD.setAttribute('text', {value: "AGL: " + Math.round(this.body.position.y) })
+    if(this.keyboardControls.isPressed('Space') && this.data.fuelLevel > 0){
+
+      this.data.fuelLevel = this.data.fuelLevel - 1;
+      this.fuelHUD.setAttribute('text', { value: "Fuel: " + this.data.fuelLevel });
+
+      if(this.movementControls.velocity.y < this.data.thrusterVelocityMax){
+        this.thrusterEngagedSecs = this.thrusterEngagedSecs + (timeDelta/1000)
+        this.movementControls.velocity.y = (8 * this.thrusterEngagedSecs);
+      }else{
+        this.movementControls.y = this.data.thrusterVelocityMax;
+      }
+
+    }else{
+      this.thrusterEngagedSecs = 0;
+      //this.movementControls.velocity.y = 0;
+    }
+  }
+
+});
