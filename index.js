2bf054e1da45e2d3f5a1cc913821239f6109a540
Steve Lewis <steve@decodingsteve.com>
2018-10-20 18:55:59 -0700

    ES6

---
+++ index.js
@@ -0,0 +1,6 @@
+require('aframe');
+require('aframe-rain');
+require('aframe-physics-system');
+require('aframe-environment-component');
+require('./components/landing-pad.js');
+require('./components/thrusters.js');
